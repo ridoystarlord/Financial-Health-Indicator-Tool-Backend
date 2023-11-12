b9e5a125c8d1524a7570f29810a1c16385787f06
unknown <ridoy.starlord@gmail.com>
2023-11-12 18:55:49 +0600

    added types for the company data in get current user

--- src/graphql/User/queries.ts
+++ src/graphql/User/queries.ts
@@ -1,4 +1,4 @@
 export const queries = `
-    getUserToken(email:String!,password:String!):UserLoginResponse
-    getUser:User
+    getUserToken(email:String!,password:String!): UserLoginResponse
+    getUser: User
 `;
--- src/graphql/User/typedefs.ts
+++ src/graphql/User/typedefs.ts
@@ -1,17 +1,23 @@
 export const typeDefs = `
+    type Company{
+        id: ID!
+        companyName: String!
+    }
     type User{
         id: ID!
         name: String!
         email: String!
+        company: Company
     }
     type CreateUserResponse{
         success: Boolean!
         message: String!
         error: String
     }
-      type UserLoginResponse{
+    type UserLoginResponse{
         success: Boolean!
         message: String!
         token: String!
     }
+    
 `;
